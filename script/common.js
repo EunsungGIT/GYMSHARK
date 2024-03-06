be5de64cc48cc8b38a5c43db094a19d186a5f864
EunsungGIT <spacerdt@gmail.com>
2024-03-06 17:01:36 +0900

    header 및 footer 동적 기능 작업

--- script/common.js
+++ script/common.js
@@ -1,9 +1,24 @@
 /* popup */
 const popup_close = document.querySelector('.popup .close')
 const popup = document.querySelector('.popup')
+const lang_left = document.querySelector('.lang_left')
+const lang_left_a = document.querySelector('.lang_left > a')
+const lang_left_more = document.querySelector('.lang_left > a .more')
+const lang_or = document.querySelector('.lang_sel > span')
+const lang_right = document.querySelector('.lang_right')
+const lang_right_a = document.querySelector('.lang_right > a')
+const header = document.querySelector('header')
 
 popup_close.addEventListener('click',()=>{
     popup.style.display = 'none'
+    header.style.top = '0'
+})
+lang_left.addEventListener('click',()=>{
+    lang_right_a.innerHTML = 'English'
+    lang_right.style.width = '150px'
+    lang_left.style.width = '150px'
+    lang_left_more.style.display = 'block'
+    lang_or.style.display = 'none'
 })
 
 /* header top lang */
@@ -21,6 +36,25 @@ top_a[5].addEventListener('click',()=>{
     }else{
         top_lang.style.display = 'none'
     }
+})
+
+/* header mobile menu */
+const left_icon = document.querySelectorAll('.left_icon a')
+const menu_m = document.querySelector('.menu_m')
+const menu_m_close = document.querySelector('.top_m > a')
+
+menu_m.style.display = 'none'
+left_icon[0].addEventListener('click',()=>{
+    menu_m.style.display = 'block'
+    setTimeout(()=>{
+        menu_m.style.left = '0'
+    }, 10);
+})
+menu_m_close.addEventListener('click',()=>{
+    menu_m.style.left = '-100%'
+    setTimeout(()=>{
+        menu_m.style.display = 'none'
+    }, 500);
 })
 
 /* header nav */
@@ -46,17 +80,34 @@ nav_a.forEach((t,i)=>{
 /* header search */
 const body_html = document.querySelector('body, html')
 const search_box = document.querySelector('.right_icon .search_box')
-const search_page = document.querySelector('.search_bg')
+const search_icon = document.querySelector('.right_icon .search')
+const search_bg = document.querySelector('.search_bg')
+const search_page = document.querySelector('.search_bg .search_page')
 const search_close = document.querySelector('.search_page .close')
 
-search_page.style.display = 'none'
+search_bg.style.display = 'none'
 search_box.addEventListener('click',()=>{
-    search_page.style.display = 'block'
+    search_bg.style.display = 'block'
     body_html.style.overflow = 'hidden'
+    setTimeout(()=>{
+        search_page.style.opacity = '1'
+    }, 10);
+})
+search_icon.addEventListener('click',()=>{
+    search_bg.style.display = 'block'
+    body_html.style.overflow = 'hidden'
+    setTimeout(()=>{
+        search_page.style.opacity = '1'
+    }, 10);
 })
 search_close.addEventListener('click',()=>{
-    search_page.style.display = 'none'
+    search_bg.style.opacity = '0'
     body_html.style.overflow = 'auto'
+    setTimeout(()=>{
+        search_bg.style.display = 'none'
+        search_bg.style.opacity = '1'
+        search_page.style.opacity = '0'
+    }, 500);
 })
 
 /* header cart */
---
+++ script/index.js
@@ -0,0 +1,62 @@
+/* recommend swiper */
+var swiper = new Swiper('.recommend_swiper', {
+    slidesPerView: 1.2,
+    spaceBetween: 10,
+    navigation:{
+        nextEl:'.product_next',
+        prevEl:'.product_prev'
+    },
+    breakpoints:{
+        768: {
+            slidesPerView: 4.2,
+        },
+    },
+});
+
+/* recommend */
+const product_icon = document.querySelectorAll('.product_image .icon a')
+const product = document.querySelectorAll('.product_slide')
+const product_img = document.querySelectorAll('.product_image > img')
+const size = document.querySelectorAll('.recommend_swiper .size')
+const size_a = document.querySelectorAll('.recommend_swiper .size a')
+
+product.forEach((t,i)=>{
+    t.addEventListener('mouseover',()=>{
+        size[i].style.bottom = '0'
+        product_img[i].src = './images/product_1_02.jpg'
+    })
+    t.addEventListener('mouseleave',()=>{
+        size[i].style.bottom = '-150px'
+        product_img[i].src = './images/product_1_01.jpg'
+    })
+})
+product_icon[0].addEventListener('click',()=>{
+    cart_bg.style.display = 'block'
+    body_html.style.overflow = 'hidden'
+    setTimeout(()=>{
+        cart_bg.style.opacity = '1'
+    }, 10);
+    setTimeout(()=>{
+        cart_page.style.right = '0'
+    }, 100);
+})
+for(let i of size_a){
+    i.addEventListener('click',()=>{
+        cart_bg.style.display = 'block'
+        body_html.style.overflow = 'hidden'
+        setTimeout(()=>{
+            cart_bg.style.opacity = '1'
+        }, 10);
+        setTimeout(()=>{
+            cart_page.style.right = '0'
+        }, 100);
+    })
+}
+cart_bg.addEventListener('click',()=>{
+    cart_bg.style.opacity = '0'
+    body_html.style.overflow = 'auto'
+    cart_page.style.right = '-500px'
+    setTimeout(()=>{
+        cart_bg.style.display = 'none'
+    }, 500);
+})
